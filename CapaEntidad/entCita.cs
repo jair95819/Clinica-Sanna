67e7c9262af4bd50f9d05c83c471a63550361f21
jair <jairestebanca@gmail.com>
2025-05-28 00:18:03 -0500

    added entCita model for medical bookings

---
+++ CapaEntidad/entCita.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class entCita
+    {
+        public int idCita{ get; set; }
+
+        public DateTime fecCreacion { get; set; }
+    }
+}
