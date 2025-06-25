424a4220e83a4d269c24759b4bc0ab7f0ea6b2d5
Dante <dantechuquipa@gmail.com>
2025-06-25 01:58:24 -0500

    se agrego mantenedor ciudad

---
+++ CapaEntidad/entCiudad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class entCiudad
+    {
+        public int idCiudad { get; set; }
+        public string nombre { get; set; }
+    }
+}
