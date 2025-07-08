c2364e063211f18936f2f0f372d6965c752cc706
Matias_Vasquez <n00317006@upn.pe>
2025-07-08 16:08:19 -0500

    entusuario

---
+++ CapaEntidad/entUsuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class entUsuario
+    {
+        public int idUsuario { get; set; }
+        public string usuario { get; set; }
+        public string contraseña { get; set; }
+        public string tipoUsuario { get; set; } // Cliente o Admin
+        public bool estado { get; set; }
+    }
+}
