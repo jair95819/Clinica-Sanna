b7aa8ffe7c7c2959a84c9b2afebb7e5fe9f791b6
Matias_Vasquez <n00317006@upn.pe>
2025-07-10 01:21:46 -0500

    especialidad listar, insertar

---
+++ VentaMueble/Controllers/MantenedorEspecialidadController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using CapaEntidad;
+using CapaLogica;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VentaMueble.Controllers
+{
+    public class MantenedorEspecialidadController : Controller
+    {
+        private readonly ILogger<MantenedorEspecialidadController> _logger;
+
+        public MantenedorEspecialidadController(ILogger<MantenedorEspecialidadController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult ListarEspecialidad()
+        {
+            List<entEspecialidad> lista = logEspecialidad.Instancia.ListarEspecialidad();
+            ViewBag.Lista = lista;
+            return View(lista);
+        }
+        [HttpGet]
+        public IActionResult InsertarEspecialidad()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult InsertarEspecialidad(entEspecialidad e)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    bool inserta = logEspecialidad.Instancia.InsertarEspecialidad(e);
+                    if (inserta)
+                    {
+                        TempData["RegistroExitoso"] = "¡Especialidad registrada exitosamente!";
+                        return RedirectToAction("ListarEspecialidad");
+                    }
+                    else
+                    {
+                        ViewBag.Error = "No se pudo registrar la especialidad.";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = "Error al registrar la especialidad: " + ex.Message;
+            }
+            return View(e);
+        }
+    }
+}
