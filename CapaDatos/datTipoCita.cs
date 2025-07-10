32853eb41b509bef9b5e52e2b992b22cb97364a8
jair <jairestebanca@gmail.com>
2025-07-10 18:16:11 -0500

    añadido TipoCita

---
+++ CapaDatos/datTipoCita.cs
@@ -0,0 +1,53 @@
+﻿using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class datTipoCita
+    {
+        #region Singleton
+        private static readonly datTipoCita instancia = new datTipoCita();
+        public static datTipoCita Instancia => instancia;
+        #endregion
+
+        #region Método
+        public List<entTipoCita> ListarTipoCita()
+        {
+            List<entTipoCita> lista = new List<entTipoCita>();
+            SqlCommand cmd = null;
+
+            try
+            {
+                SqlConnection cn = conexion.Instancia.Conectar();
+                cmd = new SqlCommand("spListarTipoCita", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cn.Open();
+
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    var tu = new entTipoCita
+                    {
+                        id = Convert.ToInt32(dr["TipocitaID"]),
+                        nombre = Convert.ToString(dr["Nombre"])
+                    };
+                    lista.Add(tu);
+                }
+            }
+            catch (Exception e)
+            { throw e; }
+            finally
+            { cmd?.Connection?.Close(); }
+
+            return lista;
+        }
+
+        #endregion
+    }
+}
---
+++ CapaLogica/logTipoCita.cs
@@ -0,0 +1,25 @@
+﻿using CapaDatos;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogica
+{
+    public class logTipoCita
+    {
+        #region Singleton
+        private static readonly logTipoCita instancia = new logTipoCita();
+        public static logTipoCita Instancia => instancia;
+        #endregion
+
+        #region Métodos
+        public List<entTipoCita> ListarTipoCita()
+        {
+            return datTipoCita.Instancia.ListarTipoCita();
+        }
+        #endregion
+    }
+}
